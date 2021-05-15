ddb016234e357476d00422ef32229f552cb424ac
Fullajtar <fullajtar.dominik@gmail.com>
2021-05-15 11:02:03 +0200

    trying to add image to expense

--- src/expense/expense.controller.ts
+++ src/expense/expense.controller.ts
@@ -1,9 +1,37 @@
 import {Expense} from "./expense.entity";
-import {Body, Controller, Get, Param, ParseIntPipe, Post, Query, Render, ValidationPipe} from "@nestjs/common";
+import {
+    Body,
+    Controller,
+    Get,
+    Param,
+    ParseIntPipe,
+    Post,
+    Query,
+    Render, UploadedFile,
+    UseInterceptors,
+    ValidationPipe
+} from "@nestjs/common";
 import {ExpenseService} from "./expense.service";
 import {GetInvoicesFilterDto} from "../invoices/dto/get-invoices-filter.dto";
 import {CreateExpenseDto} from "./dto/create-expense.dto";
 import {Company} from "../company/company.entity";
+import {FileInterceptor} from "@nestjs/platform-express";
+import {diskStorage} from 'multer';
+import {v4 as uuidv4} from 'uuid';
+import * as path from "path";
+import {Observable, of} from "rxjs";
+
+export const storage = {
+    storage: diskStorage({
+        destination: './uploads/expenseImages',
+        filename: (req, expenseFile, cb) => {
+            const filename: string = path.parse(expenseFile.originalname).name.replace(/\s/g, "") + uuidv4();
+            const extenstion: string = path.parse(expenseFile.originalname).ext;
+
+            cb(null, `${filename}${extenstion}`)
+        }
+    })
+}
 
 @Controller('expense')
 export class ExpenseController{
@@ -51,16 +79,17 @@ export class ExpenseController{
 
     @Post("/:id")
     @Render('expense/detail-expense.hbs')
+    @UseInterceptors(FileInterceptor('expenseFile', storage))
     editExpense(
         @Param('id', ParseIntPipe) id: number,
+        @UploadedFile() expenseFile,
         @Body() createExpenseDto: CreateExpenseDto,
     ) :Promise<Expense>{
+        console.log(expenseFile)
         const company = new Company();
         company.id = 1;
         return this.expenseService.editExpense(id, company, createExpenseDto);
     }
-
-
 
 
 }
